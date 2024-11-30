7b770c249613370f7c65af2187909e94a0b40e44
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-12-01 01:21:15 +0500

    code style

--- routes/api.php
+++ routes/api.php
@@ -9,22 +9,19 @@ use App\Modules\Users\UsersList\Presentation\Controllers\UsersListController;
 use Illuminate\Support\Facades\Route;
 
 
-
-Route::middleware('throttle:global')->group(function(){
+Route::middleware('throttle:global')->group(function () {
     Route::prefix('v1')->group(function () {
         Route::post('login', [LoginController::class, 'login']);
         Route::post('register', [RegisterController::class, 'register']);
-
         Route::middleware('auth:sanctum')->group(function () {
             Route::get('users', [UsersListController::class, 'list']);
             Route::get('chats', [ChatController::class, 'getChatList']);
             Route::post('chats', [ChatController::class, 'createChat']);
-            Route::middleware(ChatAccessMiddleware::class)->group(function(){
+            Route::middleware(ChatAccessMiddleware::class)->group(function () {
                 Route::get('messages', [MessageController::class, 'getMessages']);
                 Route::post('messages', [MessageController::class, 'postMessage']);
                 Route::get('message', [MessageController::class, 'getFullTextMessage']);
             });
         });
     });
-
 });
