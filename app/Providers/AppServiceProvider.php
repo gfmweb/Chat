6f4304c6f5d16ababe3a9e0066c16a7edd5f5897
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-12-01 00:43:21 +0500

    RateLimit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-
+        RateLimiter::for('global', function ($request) {
+            return Limit::perSecond(10, 1)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -8,23 +8,23 @@ use App\Modules\Users\Register\Presentation\Controllers\RegisterController;
 use App\Modules\Users\UsersList\Presentation\Controllers\UsersListController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/user', function (Request $request) {
-//    return $request->user();
-//})->middleware('auth:sanctum');
 
 
-Route::prefix('v1')->group(function () {
-    Route::post('login', [LoginController::class, 'login']);
-    Route::post('register', [RegisterController::class, 'register']);
+Route::middleware('throttle:global')->group(function(){
+    Route::prefix('v1')->group(function () {
+        Route::post('login', [LoginController::class, 'login']);
+        Route::post('register', [RegisterController::class, 'register']);
 
-    Route::middleware('auth:sanctum')->group(function () {
-        Route::get('users', [UsersListController::class, 'list']);
-        Route::get('chats', [ChatController::class, 'getChatList']);
-        Route::post('chats', [ChatController::class, 'createChat']);
-        Route::middleware(ChatAccessMiddleware::class)->group(function(){
-            Route::get('messages', [MessageController::class, 'getMessages']);
-            Route::post('messages', [MessageController::class, 'postMessage']);
-            Route::get('message', [MessageController::class, 'getFullTextMessage']);
-       });
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('users', [UsersListController::class, 'list']);
+            Route::get('chats', [ChatController::class, 'getChatList']);
+            Route::post('chats', [ChatController::class, 'createChat']);
+            Route::middleware(ChatAccessMiddleware::class)->group(function(){
+                Route::get('messages', [MessageController::class, 'getMessages']);
+                Route::post('messages', [MessageController::class, 'postMessage']);
+                Route::get('message', [MessageController::class, 'getFullTextMessage']);
+            });
+        });
     });
+
 });
