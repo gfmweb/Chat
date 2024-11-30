b82298489256b79b3af4121ea6002d25a905f387
Гофман Юрий Александрович <y.gofman@nactacredit.com>
2024-12-01 00:53:04 +0500

    Описание переменной email в Request

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\RateLimit;
 
-Route::get('/test', [\App\Http\Controllers\TypokController::class, 'index'])
-    ->middleware(RateLimit::class)->name('home');
+
